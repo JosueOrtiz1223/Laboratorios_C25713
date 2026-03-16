4b2484eb6dc00f4177ef2b18a6f2bbd102334d11
Josue.ortiz <Josue.ortiz@ucr.ac.cr>
2026-03-16 02:04:22 -0600

    creamos el archivo .Net incial con su version MVC

---
+++ Laboratorio1_C25713/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Laboratorio1_C25713.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
